ea008c7e1c356061abb2e70ecab98e7993fe4c48
Momenwael21 <waelmomen42@gmail.com>
2022-10-17 17:10:55 +0200

    Hello

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// change nav background
+let fixedNav = document.querySelector(".navbar");
+let navContainer = document.querySelector(".navbar .container");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    fixedNav.classList.add("active");
+
+    navContainer.classList.add("container-fluid");
+    navContainer.classList.remove("container");
+  } else {
+    fixedNav.classList.remove("active");
+
+    navContainer.classList.remove("container-fluid");
+    navContainer.classList.add("container");
+  }
+});
+
+document
+  .querySelector(".navbar-toggler")
+  .addEventListener("click", function () {
+    if (this.ariaExpanded) {
+      fixedNav.classList.add("active");
+    } else if (!this.ariaExpanded) {
+      fixedNav.classList.remove("active");
+    }
+  });
+
+// change hadiths
+let hadithContiner = document.querySelector(".hadith-container");
+let hadithNumber = document.querySelector(".hadith-number");
+
+let btnPrev = document.querySelector(".hadith .buttons .btn-previous");
+let btnNext = document.querySelector(".hadith .buttons .btn-next");
+
+let hadithIndex = 0;
+
+let hadiths = fetch(
+  "https://ahadith-api.herokuapp.com/api/ahadith/all/ar-tashkeel"
+).then((resp) => resp.json());
+// .then((resp) => {
+//   resp.AllChapters.length = 300;
+//   return resp;
+// });
+
+hadithChange();
+
+btnNext.addEventListener("click", function () {
+  hadithChange();
+});
+
+btnPrev.addEventListener("click", function () {
+  hadithIndex = hadithIndex - 2;
+  hadithChange();
+});
+
+function hadithChange() {
+  hadithIndex > 1896 ? (hadithIndex = 0) : hadithIndex;
+  hadithIndex < 0 ? (hadithIndex = 1896) : hadithIndex;
+  hadiths.then((data) => {
+    hadithContiner.textContent = data.AllChapters[hadithIndex].Ar_Text;
+    hadithNumber.textContent = `${data.AllChapters[hadithIndex].Hadith_ID}/${data.AllChapters.length}`;
+    hadithIndex++;
+  });
+}
+
+setInterval(hadithChange, 40000);
